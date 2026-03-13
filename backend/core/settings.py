c1211c73de6d765bf1f00fcd830fb65441751ef8
jmt9454 <jmt9454@uncw.edu>
2026-03-13 09:18:02 -0400

    feat: add initial API structure with models, views, and configuration

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -40,9 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'api',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +54,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
 ]
 
 ROOT_URLCONF = 'core.urls'
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api.views import hello_vue
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/hello/', hello_vue, name='hello_vue'),
 ]
