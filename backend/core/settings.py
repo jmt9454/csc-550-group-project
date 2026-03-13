f415ea28de53e095be7366274a6c0cd61d958727
jmt9454 <jmt9454@uncw.edu>
2026-03-13 10:00:25 -0400

    feat: added auth routes for api

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -61,6 +61,12 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173"
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
 ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from api.views import health_check
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/health/', health_check, name='health_check'),
 ]
